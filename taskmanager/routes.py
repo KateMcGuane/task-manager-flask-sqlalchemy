70d7e7b241318325ef51891d7ce37ba9ebb6d4b3
Kate Mcguane <katemcguane@hotmail.com>
2024-07-04 15:57:28 +0000

    Fix typos

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -52,7 +52,7 @@ def add_task():
             task_description=request.form.get("task_description"),
             is_urgent=bool(True if request.form.get("is_urgent") else False),
             due_date=request.form.get("due_date"),
-            category_id =request.form.get("category_id")
+            category_id=request.form.get("category_id")
         )
         db.session.add(task)
         db.session.commit()
