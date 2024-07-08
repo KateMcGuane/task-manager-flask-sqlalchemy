704e8de8e43cfc21a68a1671d059b15bf825bec3
Kate Mcguane <katemcguane@hotmail.com>
2024-07-08 11:07:35 +0000

    Remove questionable line from routes.py

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -71,7 +71,6 @@ def edit_task(task_id):
         task.due_date = request.form.get("due_date")
         task.category_id = request.form.get("category_id")
         db.session.commit()
-
     return render_template("edit_task.html", task=task, categories=categories)
 
 
